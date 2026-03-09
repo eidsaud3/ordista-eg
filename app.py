0f28db0297c00325a7b5cb5f99fbb58f2558ac11
eidsaud3 <eid_saud3@outlook.com>
2026-03-09 19:55:59 +0200

    insert form function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 import json
 
 app = Flask(__name__)
@@ -7,12 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# usrer - > json file
-# product - > json file
-# order - > json file
-# cart - > json file
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 def account():
-    return render_template('account.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # Here you can add code to validate the email and password
+        # For example, you can check if they match a predefined set of credentials
+        if email == 'admin@example.com' and password == 'password':
+            return render_template('account.html', email=email)
+        else:
+            error_message = 'Invalid email or password. Please try again.'
+            return render_template('index.html', error=error_message)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,6 @@
+a = 1
+b = "mohamed"
+
+# variable is a container for storing data values
+# variable name rules       
+print(type(a))
